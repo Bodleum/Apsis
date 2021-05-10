3745b27fce1f4b5aff8f327cc0ef6f30cfc5104a
Bodleum <bodleum.phone@gmail.com>
2021-05-10 11:55:35 +0100

    👌 IMPROVED: Opitmised ortho matirx creation

--- Apsis/src/Apsis/Renderer/Cam.cpp
+++ Apsis/src/Apsis/Renderer/Cam.cpp
@@ -15,10 +15,10 @@ namespace A {
 		m_Proj = Eigen::Matrix4f::Zero();
 		m_Proj(0, 0) = 2 / (right - left);
 		m_Proj(1, 1) = 2 / (top - bottom);
-		m_Proj(2, 2) = -1 * 2 / (z_far - z_near);
-		m_Proj(0, 3) = -1 * (right + left) / (right - left);
-		m_Proj(1, 3) = -1 * (top + bottom) / (top - bottom);
-		m_Proj(2, 3) = -1 * (z_far + z_near) / (z_far - z_near);
+		m_Proj(2, 2) = 2 / (z_near - z_far);
+		m_Proj(0, 3) = (right + left) / (left - right);
+		m_Proj(1, 3) = (top + bottom) / (bottom - top);
+		m_Proj(2, 3) = (z_far + z_near) / (z_near - z_far);
 		m_Proj(3, 3) = 1;
 	}
 
