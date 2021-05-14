0976a84e3678102f409c20c69ef70f8026ca8eea
Bodleum <bodleum.phone@gmail.com>
2021-05-14 22:28:19 +0100

    🧹 OpenGLWindow and Translators

--- Apsis/src/OpenGL/OpenGLWindow.cpp
+++ Apsis/src/OpenGL/OpenGLWindow.cpp
@@ -16,15 +16,9 @@
 #include "OpenGLHeaders.h"
 	//#include <glad/glad.h>
 	//#include <GLFW/glfw3.h>
-
-
-
 #include "Translators.h"
-#pragma message ("TODO")
-
-
-
-
+	//#include "Apsis/Core/Input/InputCodes.h"
+	//	#include <iostream>
 
 namespace A {
 
@@ -96,17 +90,17 @@ namespace A {
 					{
 						case GLFW_PRESS:
 						{
-							EventDispatcher::SendEvent(MakeShared<KeyDownEvent>(GLFWKeyCodeTranslator[key], false));
+							EventDispatcher::SendEvent(MakeShared<KeyDownEvent>(GLFWKeyCodeToKeyCode(key), false));
 							break;
 						}
 						case GLFW_REPEAT:
 						{
-							EventDispatcher::SendEvent(MakeShared<KeyDownEvent>(GLFWKeyCodeTranslator[key], true));
+							EventDispatcher::SendEvent(MakeShared<KeyDownEvent>(GLFWKeyCodeToKeyCode(key), true));
 							break;
 						}
 						case GLFW_RELEASE:
 						{
-							EventDispatcher::SendEvent(MakeShared<KeyUpEvent>(GLFWKeyCodeTranslator[key]));
+							EventDispatcher::SendEvent(MakeShared<KeyUpEvent>(GLFWKeyCodeToKeyCode(key)));
 							break;
 						}
 					}
@@ -115,7 +109,7 @@ namespace A {
 
 			glfwSetCharCallback(m_WindowHandle, [](GLFWwindow* window, unsigned int codepoint)
 				{
-					EventDispatcher::SendEvent(MakeShared<KeyCharEvent>(GLFWKeyCodeTranslator[codepoint], (char)codepoint, false));
+					EventDispatcher::SendEvent(MakeShared<KeyCharEvent>(Key::Unknown, (char)codepoint, false));
 				}
 			);
 
@@ -127,14 +121,14 @@ namespace A {
 						{
 							double x, y;
 							glfwGetCursorPos(window, &x, &y);
-							EventDispatcher::SendEvent(MakeShared<MouseButtonDownEvent>((int)x, (int)y, GLFWMouseButtonTranslator[button]));
+							EventDispatcher::SendEvent(MakeShared<MouseButtonDownEvent>((int)x, (int)y, GLFWMouseButtonToMouseCode(button)));
 							break;
 						}
 						case GLFW_RELEASE:
 						{
 							double x, y;
 							glfwGetCursorPos(window, &x, &y);
-							EventDispatcher::SendEvent(MakeShared<MouseButtonUpEvent>((int)x, (int)y, GLFWMouseButtonTranslator[button]));
+							EventDispatcher::SendEvent(MakeShared<MouseButtonUpEvent>((int)x, (int)y, GLFWMouseButtonToMouseCode(button)));
 							break;
 							break;
 						}
